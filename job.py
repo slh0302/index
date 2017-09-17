3d20065a20ae48068ffca0dab0c267b8fc27e0b0
slh@megatron <slh@megatron>
2017-09-17 18:30:15 +0800

    file change

--- job.py
+++ job.py
@@ -3,27 +3,25 @@ import shutil
 import time
 from subprocess import Popen, PIPE
 
-file_name = ["total.list_0", "total.list_1", "total.list_2"
-    , "total.list_3", "total.list_4", "total.list_5", "total.list_6"]
+file_name = ["total_list_0", "total_list_1", "total_list_2"
+    , "total_list_3", "total_list_4", "total_list_5"
+    , "total_list_6", "total_list_7", "total_list_8"
+    , "total_list_9", "total_list_10", "total_list_11"]
 
-file_count = [2000000, 2000000, 2000000, 2000000, 2000000, 2000000, 251928]
-file_name_prefix = "/home/slh/extract_file/"
-target_prefix = "/home/slh/Vechile_Search/libfaster_rcnn_cpp"
+file_count = [2000000, 2000000, 2000000, 2000000, 2000000
+    , 2000000, 2000000, 2000000, 2000000, 2000000, 2000000, 355764]
+file_name_prefix = "/home/slh/data/new_data_float/"
 file_list_name = "/home/slh/Vechile_Search/libfaster_rcnn_cpp/examples/_temp/file_list"
 num = 0
 beg = time.time()
 for index in file_name:
-    if os.path.isfile(file_list_name):
-        os.remove(file_list_name)
-        shutil.copyfile(file_name_prefix + index, file_list_name)
-    else:
-        shutil.copyfile(file_name_prefix + index, file_list_name)
-
+    if not os.path.exists(file_name_prefix + index):
+        print file_name_prefix + index, " not exist"
+        exit()
     lines = file_count[num]
-    print os.path.isfile(file_list_name)
 
     exec_script = "./bin/DoIndex -i " + file_name_prefix + index + " " + str(lines) + \
-                  " 280 ~/data_model_120000_2000000_" + str(num) + " GPU 0 "
+                  " 280 ~/data_float_2000000_" + str(num) + " GPU 0 "
 
     print exec_script
     resp = Popen(exec_script, shell=True, stdout=PIPE, stderr=PIPE).stdout.readlines()
